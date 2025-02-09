423695046ffe35b9cbb5798452a6078365fa60ff
TomerFisher <fisher.tomer@gmail.com>
2025-02-09 10:44:17 +0200

    changed login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,13 +4,14 @@ import Input from "@/components/input";
 import { ChangeEvent, useCallback, useState } from "react";
 
 export default function AuthPage() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [variant, setVariant] = useState("password");
+  const [variant, setVariant] = useState("login");
 
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) =>
-      currentVariant === "password" ? "code" : "password",
+      currentVariant === "login" ? "register" : "login",
     );
   }, []);
 
@@ -24,38 +25,44 @@ export default function AuthPage() {
           <div className="mt-2 w-full min-w-96 rounded bg-black bg-opacity-70 px-16 py-16 sm:w-2/5 sm:max-w-md">
             <h2 className="mb-8 text-4xl font-semibold text-white">Sign In</h2>
             <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  label="Name"
+                  id="name"
+                  value={name}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                    setName(event.currentTarget.value)
+                  }
+                />
+              )}
               <Input
-                label="Email or mobile number"
+                label="Email"
                 id="email"
                 value={email}
                 onChange={(event: ChangeEvent<HTMLInputElement>) =>
                   setEmail(event.currentTarget.value)
                 }
               />
-              {variant === "password" && (
-                <Input
-                  label="Password"
-                  id="password"
-                  value={password}
-                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                    setPassword(event.currentTarget.value)
-                  }
-                />
-              )}
+              <Input
+                label="Password"
+                id="password"
+                value={password}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  setPassword(event.currentTarget.value)
+                }
+              />
               <button className="mt-2 block w-full rounded-md bg-red-600 py-3 font-semibold text-white transition hover:bg-red-700 active:bg-red-900">
-                {variant === "password" ? "Sign In" : "Send Sign-In Code"}
-              </button>
-              <p className="w-full text-center text-white">OR</p>
-              <button
-                className="block w-full rounded-md bg-gray-500 py-3 font-semibold text-white opacity-80 transition hover:opacity-50 active:opacity-40"
-                onClick={toggleVariant}
-              >
-                {variant === "password" ? "Use a Sign-In Code" : "Use Password"}
+                {variant === "login" ? "Sign In" : "Sign Up"}
               </button>
               <p className="text-neutral-400">
-                New to Netflix?{" "}
-                <span className="cursor-pointer font-bold text-white hover:underline">
-                  Sign up now
+                {variant === "login"
+                  ? "New to Netflix?"
+                  : "Already Have an Account?"}{" "}
+                <span
+                  className="cursor-pointer font-bold text-white hover:underline"
+                  onClick={toggleVariant}
+                >
+                  {variant === "login" ? "Sign up now" : "Login"}
                 </span>
                 .
               </p>
