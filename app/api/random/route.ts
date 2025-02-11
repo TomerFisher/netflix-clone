095e039f8b2c149ab6c81f5aed5644fb5f7c720e
TomerFisher <fisher.tomer@gmail.com>
2025-02-11 17:24:51 +0200

    improve try catch block

--- app/api/random/route.ts
+++ app/api/random/route.ts
@@ -3,15 +3,15 @@ import { prisma } from "@/lib/prismadb";
 import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET() {
-  try {
-    const session = await getServerSession(authOptions);
-    if (!session) {
-      return Response.json(
-        { message: "You must be logged in." },
-        { status: 401 },
-      );
-    }
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return Response.json(
+      { message: "You must be logged in." },
+      { status: 401 },
+    );
+  }
 
+  try {
     const moviesCount = await prisma.movie.count();
     const randomIndex = Math.floor(Math.random() * moviesCount);
     const randomMovie = await prisma.movie.findFirst({
